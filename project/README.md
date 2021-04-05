92deb0dfe491cec9956b879afe42f29e5ebc6e24
Andrew Corum <amcorum@iu.edu>
2021-04-05 05:29:06 -0400

    project: Function simple rubiks/akari classifier, using Bucket of Visual Works and 1 nearest neighbor

--- project/README.md
+++ project/README.md
@@ -15,6 +15,9 @@ Andrew Corum
 * https://docs.opencv.org/
     * https://docs.opencv.org/3.4/da/df5/tutorial_py_sift_intro.html
     * https://docs.opencv.org/3.4/dc/dc3/tutorial_py_matcher.html
+    * https://docs.opencv.org/master/d1/d5c/tutorial_py_kmeans_opencv.html
 * Bag of Visual Words:
     * https://en.wikipedia.org/wiki/Bag-of-words_model_in_computer_vision
     * https://medium.com/@aybukeyalcinerr/bag-of-visual-words-bovw-db9500331b2f
+* Searching for array in a 2d array:
+    * https://www.w3resource.com/python-exercises/numpy/python-numpy-exercise-171.php
--- project/classifier.py
+++ project/classifier.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+import math
 import numpy as np
 import os
 
 from scipy.spatial import distance
 from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 import cv2 as cv
 
@@ -16,18 +19,35 @@ def image_name_and_class(loc):
         labels += [label] * len(os.listdir(path))
     return images, labels
 
-def orb_features(images):
-    descripts = None
-    maxfeat, nfeat = 500, 100
-    orb = cv.ORB_create(nfeatures=maxfeat)
+def sift_features(images):
+    descripts = []
+    sift = cv.SIFT_create()
     for img in images:
-        keypts, des = orb.detectAndCompute(img, None)
-        descripts = des[:nfeat] if descripts is None else descripts + des[:nfeat]
+        keypts, des = sift.detectAndCompute(img, None)
+        descripts.append(des)
     return descripts
 
 def cluster_words(k, descripts):
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(descripts)
-    return kmeans.cluster_centers_
+    #  kmeans = KMeans(n_clusters=k, random_state=0).fit(descripts)
+    #  return kmeans.cluster_centers_
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    flags = cv.KMEANS_RANDOM_CENTERS
+    compactness, _, centers = cv.kmeans(
+            np.array(descripts), k, None, criteria, 10, flags)
+    radius = math.sqrt(compactness / len(descripts))
+    return centers, radius
+
+def build_bovw(images, words, thresh):
+    descripts = sift_features(images)
+    bags = []
+    for img, des in zip(images, descripts):
+        hist = [0] * len(words)
+        for feature in des:
+            for i in range(len(words)):
+                if distance.euclidean(feature, words[i]) < thresh:
+                    hist[i] += 1
+        bags.append(hist)
+    return bags
 
 def NN(X_test, X_train, y_train):
     y_pred = []
@@ -45,14 +65,23 @@ def NN(X_test, X_train, y_train):
 if __name__ == "__main__":
     # Grab data
     img, y = image_name_and_class('./data')
+    enc = LabelEncoder()
+    y = enc.fit_transform(y)
     img_train, img_test, y_train, y_test = train_test_split(
             img, y, test_size=0.33, random_state=0, stratify=y)
 
     #  # Train classifier
-    descripts = orb_features(img_train)
-    visual_words = cluster_words(100, descripts)
-    #  X_train = build_bovw(img_train, visual_words)
-#  
-    #  # Test classifier
-    #  X_test = build_bovw(img_test, visual_words) 
-    #  y_pred = NN(X_test, X_train, y_train)
+    descripts = sift_features(img_train)
+    visual_words, radius = cluster_words(100, [val for row in descripts for val in row])
+    X_train = build_bovw(img_train, visual_words, radius)
+  
+    # Test classifier
+    X_test = build_bovw(img_test, visual_words, radius) 
+    y_pred = NN(X_test, X_train, y_train)
+
+    # Evaluate model
+    print("Accuracy: {}".format(accuracy_score(y_test, y_pred)))
+    print("Confusion Matrix:\n{}".format(confusion_matrix(y_test, y_pred)))
+
+    img = [cv.imread('test2.png', 0)]
+    print(enc.inverse_transform(NN(build_bovw(img, visual_words, radius), np.concatenate((X_train, X_test), axis=0), np.concatenate((y_train, y_test), axis=0))))
