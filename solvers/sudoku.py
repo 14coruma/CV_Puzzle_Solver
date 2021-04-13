2e1471424c1868e45f37f4aa86cc40182f001b55
Andrew Corum <amcorum@iu.edu>
2021-04-13 01:08:48 -0400

    Make sudoku code ready to use njit. However, njit does not speed things up here (maybe because I have a recursive solution?)

--- solvers/sudoku.py
+++ solvers/sudoku.py
@@ -1,9 +1,11 @@
 # Code used to solve a Sudoku puzzle, given an image
 import numpy as np
+from numba import njit
 
 NUMS = np.array([1,2,3,4,5,6,7,8,9])
 
 # Check if a sudoku board is valid (ie solved) 
+#@njit()
 def valid(board):
     for i in range(9):
         # Rows have 1..9
@@ -16,19 +18,24 @@ def valid(board):
     return True
 
 # Generate list of all legal numbers at a given position
+#@njit()
 def possible_nums(board, r, c):
     p_nums = np.copy(NUMS)
     for i in range(9):
-        # Check numbers already in row
-        p_nums = np.delete(p_nums, np.argwhere(p_nums == board[r,i]))
-        # Check numbers already in column
-        p_nums = np.delete(p_nums, np.argwhere(p_nums == board[i,c]))
-        # Check numbers already in square
+        # Replace numbers already in row
+        idx = np.argwhere(p_nums == board[r,i])
+        if len(idx)>0: p_nums[idx[0][0]] = 0
+        # Replace numbers already in column
+        idx = np.argwhere(p_nums == board[i,c])
+        if len(idx)>0: p_nums[idx[0][0]] = 0
+        # Replace numbers already in square
         sr, sc = 3*(r//3), 3*(c//3)
-        p_nums = np.delete(p_nums, np.argwhere(p_nums == board[sr+i//3, sc+i%3]))
+        idx = np.argwhere(p_nums == board[sr+i//3, sc+i%3])
+        if len(idx)>0: p_nums[idx[0][0]] = 0
     return p_nums
 
 # Given a 9x9 sudoku board, solve recursively using backtracking
+#@njit()
 def solve(board, r=0, c=0):
     # If at last position, don't recurse further
     if r >= 8 and c >= 8:
@@ -44,6 +51,7 @@ def solve(board, r=0, c=0):
     if board[r,c] != 0: return solve(np.copy(board), next_r, next_c)
     p_nums = possible_nums(board, r, c)
     for p in p_nums:
+        if p == 0: continue
         board[r,c] = p
         result = solve(np.copy(board), next_r, next_c)
         if result is not None: return result
@@ -51,14 +59,14 @@ def solve(board, r=0, c=0):
 
 if __name__ == "__main__":
     board = np.array([
-        [0,6,0,0,0,4,0,0,3],
-        [0,0,0,3,0,0,0,5,0],
-        [3,4,0,0,1,9,0,0,7],
-        [0,0,5,0,0,0,4,0,0],
-        [7,0,0,8,0,0,0,0,2],
-        [0,1,9,0,0,0,0,3,0],
-        [0,2,6,0,0,8,0,0,5],
-        [8,5,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,9,0]
+        [4,7,9,0,0,5,0,0,0],
+        [0,0,0,0,3,0,0,0,8],
+        [0,0,0,0,0,0,0,6,0],
+        [3,4,0,0,0,0,0,0,1],
+        [0,0,6,0,5,0,0,0,9],
+        [8,0,0,0,0,0,0,0,6],
+        [0,0,0,0,0,0,4,2,7],
+        [0,0,7,0,0,0,0,0,0],
+        [0,0,0,1,9,0,0,0,0]
     ])
     print(solve(board))
