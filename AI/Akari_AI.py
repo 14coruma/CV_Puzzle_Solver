c9a7838e48438b672f313dcd4537f04363abe6d2
Andrew Corum <amcorum@iu.edu>
2021-04-26 02:54:37 -0400

    Working akari solver... but WAY TOO SLOW! Need better approach

--- AI/Akari_AI.py
+++ AI/Akari_AI.py
@@ -1,8 +1,11 @@
 # Code used to solve an Akari puzzle, given a parsed board
 import numpy as np
 
+SIZE = 4
+COUNT = 0
+
 # General board notation:
-# 10x10 grid
+# SIZExSIZE grid
 # Numbers 0-4 represent number of adjacent lights
 # Number -1 represents an empty space
 # Number 9 represents a wall
@@ -12,42 +15,97 @@ import numpy as np
 # Check if an akari board is valid/solved
 def valid(board):
     # Check that lights and adjacent numbers match up
-    for y in range(10):
-        for x in range(10):
-            if board[y,x] in [-1, 8, 9]: continue
+    for y in range(SIZE):
+        for x in range(SIZE):
+            if board[y,x] in [-2, -1, 8, 9]: continue
             light_count = 0
             if y>0 and board[y-1,x] == 8: light_count += 1
-            if y<9 and board[y+1,x] == 8: light_count += 1
+            if y<SIZE-1 and board[y+1,x] == 8: light_count += 1
             if x>0 and board[y,x-1] == 8: light_count += 1
-            if x<9 and board[y,x+1] == 8: light_count += 1
-            if light_count != board[y,x]:
-                print(y, x)
-                return False
+            if x<SIZE-1 and board[y,x+1] == 8: light_count += 1
+            if light_count != board[y,x]: return False
     # Check that lights do not shine on each other
-    # Check that all spaces are lit, by marking them with -2
+    # Mark lit spaces with -2
     lights = np.where(board == 8)
     for y,x in zip(lights[0], lights[1]):
         # Shine up
         for i in reversed(range(y)):
             if board[i,x] == 8: return False
-            elif board[i,x] > 0: break
+            elif board[i,x] >= 0: break
             else: board[i,x] = -2
         # Shine down
-        for i in range(y+1,10):
+        for i in range(y+1,SIZE):
             if board[i,x] == 8: return False
-            elif board[i,x] > 0: break
+            elif board[i,x] >= 0: break
             else: board[i,x] = -2
         # Shine right
-        for i in range(x+1,10):
+        for i in range(x+1,SIZE):
             if board[y,i] == 8: return False
-            elif board[y,i] > 0: break
+            elif board[y,i] >= 0: break
             else: board[y,i] = -2
         # Shine left
         for i in reversed(range(x)):
             if board[y,i] == 8: return False
-            elif board[y,i] > 0: break
+            elif board[y,i] >= 0: break
             else: board[y,i] = -2
+    # Check that all spaces are lit
+    for y in range(SIZE):
+        for x in range(SIZE):
+            if board[y,x] == -1: return False
     return True
+
+# Mark lit spaces if light at position y,x
+# If lighting another light, then return None
+def mark_lights(board, y, x):
+    # Shine up
+    for i in reversed(range(y)):
+        if board[i,x] == 8: return None
+        elif board[i,x] >= 0: break
+        else: board[i,x] = -2
+    # Shine down
+    for i in range(y+1,SIZE):
+        if board[i,x] == 8: return None
+        elif board[i,x] >= 0: break
+        else: board[i,x] = -2
+    # Shine right
+    for i in range(x+1,SIZE):
+        if board[y,i] == 8: return None
+        elif board[y,i] >= 0: break
+        else: board[y,i] = -2
+    # Shine left
+    for i in reversed(range(x)):
+        if board[y,i] == 8: return None
+        elif board[y,i] >= 0: break
+        else: board[y,i] = -2
+    board[y,x] = 8
+    return board
+
+# Given a SIZExSIZE akari board, solve recursively using backtracking
+def solve(board, r=0, c=0):
+    # If at last position, don't recurse further
+    if r >= SIZE-1 and c >= SIZE-1:
+        if valid(board): return board
+        if board[r,c] == -1: board = mark_lights(board, r, c)
+        if board is not None and valid(board): return board
+        global COUNT
+        COUNT += 1
+        return None
+    
+    next_r = r + (c+1) // SIZE
+    next_c = (c+1) % SIZE
+    # If not an empty, unlit space, move to next position
+    if board[r,c] != -1: return solve(np.copy(board), next_r, next_c)
+    # Check with a light at current position, if possible
+    board2 = mark_lights(np.copy(board), r, c)
+    if board2 is not None:
+        board2[r,c] = 8
+        result = solve(np.copy(board2), next_r, next_c)
+        if result is not None: return result
+    # Check leaving current space empty
+    result = solve(np.copy(board), next_r, next_c)
+    if result is not None: return result
+    # Otherwise, no solution possible...
+    return None
 
 if __name__ == "__main__":
     board = np.array([
@@ -62,18 +120,7 @@ if __name__ == "__main__":
         [-1, -1, -1, -1, -1, -1,  9,  9,  2, -1],
         [ 9,  9,  1, -1, -1, -1, -1, -1, -1, -1]
     ])
-    #board = np.array([
-    #    [1,0,-1,8,1,-1,9,-1,8,1],
-    #    [8,-1,-1,-1,2,8,2,8,-1,1],
-    #    [-1,8,-1,1,8,-1,-1,9,-1,8],
-    #    [-1,2,8,9,-1,9,-1,9,-1,9],
-    #    [-1,9,-1,-1,-1,1,8,-1,-1,9],
-    #    [9,-1,-1,-1,9,-1,-1,-1,9,-1],
-    #    [9,-1,1,8,2,8,9,-1,1,8],
-    #    [8,-1,9,-1,-1,-1,9,-1,-1,-1],
-    #    [2,8,-1,9,-1,0,-1,-1,8,-1],
-    #    [9,-1,8,2,8,1,-1,8,2,9]
-    #])
+    board = board[:SIZE,:SIZE]
     print("Input board:\n", board)
-    print("Solution:\n")
-    print(valid(board))
+    print("Solution:\n", solve(board))
+    print(COUNT)
